47ef0a1a016203897c9f41d1422bb8186fb3f49d
Aleksey Vyatkin <avyatkin@gmail.com>
2023-08-13 14:16:09 +0300

    fixed const

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-const element = document.createElement('div');
+let element = document.createElement('div');
 document.body.appendChild(element);
 const root = createRoot(element);
 root.render(<App />);
